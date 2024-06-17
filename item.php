5a4c4a5c58bb47b9d9e3d7d1c5b144361c6b4152
AnuranDey <anurandey21@gmail.com>
2024-06-17 23:10:59 +0530

    Add files via upload

---
+++ item.php
@@ -0,0 +1,15 @@
+<?php
+class Item {
+    public $id;
+    public $title;
+    public $price;
+    public $image;
+
+    public function __construct($id, $title, $price, $image) {
+        $this->id = $id;
+        $this->title = $title;
+        $this->price = $price;
+        $this->image = $image;
+    }
+}
+?>
